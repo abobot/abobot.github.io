eb79495d658aba9cb3d65e9140e28a03cc858336
abobot <1145451169@qq.com>
2023-10-16 16:08:47 +0800

    deploied by pc

--- js/modelJS/002.js
+++ js/modelJS/002.js
@@ -5,8 +5,9 @@ import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 
 // https://codesandbox.io/s/three-jsjia-zai-3dmo-xing-forked-v6yjgy?file=/src/Scene.js
-const path = "../js/models/001/"
+const path = "../js/models/002/"
 const DecoderPath = '../js/three/jsm/libs/draco/'
+const modelName = "002.gltf"
 
 let renderer, scene, camera, controls, material;
 // let width = window.innerWidth;
@@ -40,7 +41,9 @@ function setCamera(){
         0.1,
         10000
       )
-      camera.position.z = 8
+      let p = 4
+      camera.position.set(p+2.12,p-0.47,p+0.18)
+      camera.lookAt(10,10,10)
       scene.add(camera)
 }
 
@@ -64,7 +67,7 @@ function setModel() {
     if(flag){
       objLoader
       .setPath(path)
-      .load("001.obj", (obj) => {
+      .load(modelName, (obj) => {
         const s = 0.01
         obj.scale.set(s, s, s)
         scene.add(obj)
@@ -72,7 +75,7 @@ function setModel() {
     }else{
       gltfLoader
       .setPath(path)
-      .load("001.gltf", (gltf) => {
+      .load(modelName, (gltf) => {
         const s = 50
         gltf.scene.scale.set(s, s, s)
         scene.add(gltf.scene)
@@ -98,7 +101,7 @@ function animate(){
 }
 
 function setLight() {
-    let light = 1;
+    let light = -1;
     switch(light){
       case 0:
         const spotLight = new THREE.SpotLight()
@@ -117,5 +120,19 @@ function setLight() {
         hemiLight.intensity = 10;
 				scene.add( hemiLight );
         break;
+    default:
+        const spotLight1 = new THREE.SpotLight()
+        spotLight1.position.set(10, 3, 3)
+        spotLight1.intensity = 300
+        scene.add(spotLight1)
+
+        const ambientLight1 = new THREE.AmbientLight( 0x404040 ); // soft white light
+        ambientLight1.intensity = 15;
+        scene.add( ambientLight1 );
+
+        const hemiLight1 = new THREE.HemisphereLight( 0xffffff, 0x8d8d8d, 3 );
+                hemiLight1.position.set( 0, 1, 0 );
+        hemiLight1.intensity = 3;
+                scene.add( hemiLight1 );
     }
   }
